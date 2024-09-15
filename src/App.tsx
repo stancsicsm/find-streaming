a077d075326f69fbf574de8cbae7df5af55be376
Mate Stancsics <mate.stancsics@tngtech.com>
2024-09-15 19:30:27 +0200

    feat: add country selector for streaming services

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,34 @@ import React, { useState, useEffect } from "react";
 import queryMovies from "./api/queryMovies";
 import queryProviders from "./api/queryProviders";
 import queryTrailers from "./api/queryTrailers";
+import queryCountries from "./api/queryCountries";
 import { Movie } from "./interfaces/movieInterfaces";
 import MoviesTable from "./components/moviesTable";
 import Filter from "./components/movieFilters";
 import { ProvidersResponse, Provider } from "./interfaces/providerInterfaces";
 import { TrailerResponse, Trailer } from "./interfaces/trailerInterface"
 import { Container, Row, Col } from 'react-bootstrap';
+import CountryFilter from "./components/countryFilter";
+import {Country} from "./interfaces/countryInterface";
 
 const App: React.FC = () => {
   const [streamableMovies, setStreamableMovies] = useState<Movie[]>([]);
   const [filterParams, setFilterParams] = useState<
     Map<string, string | number>
   >(new Map());
+  const [countryOptions, setCountryOptions] = useState<Country[]>([]);
+  const [selectedCountryId, setSelectedCountryId] = useState("HU");
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    queryCountries()
+      .then((countries) => {
+        setCountryOptions(countries);
+      })
+      .catch((error) => {
+        console.error("Error fetching countries:", error);
+      });
+  }, []);
 
   useEffect(() => {
     setIsLoading(true);
@@ -33,14 +48,17 @@ const App: React.FC = () => {
               .then((trailersArray: TrailerResponse[]) => {
                 const moviesWithProvidersAndTrailers: Movie[] = movies.map((movie, index) => {
                   const providers = providersArray[index];
-                  const providersHU: Provider[] = providers.results?.HU?.flatrate || [];
+                  const providersCountry: Provider[] = providers
+                    .results
+                    ?.[selectedCountryId]
+                    ?.flatrate || [];
                   const trailers: Trailer[] = trailersArray[index].results;
                   const trailerKey: string = trailers
                     .filter((trailer) => trailer.official && trailer.type === 'Trailer' && trailer.site === 'YouTube')
                     .sort((a, b) => Date.parse(b.published_at) - Date.parse(a.published_at))
                     .slice(0, 1)
                     .map((trailer) => trailer.key)[0];
-                  return {...movie, providers: providersHU, trailerKey: trailerKey };
+                  return {...movie, providers: providersCountry, trailerKey: trailerKey };
                 });
                 const streamable: Movie[] = moviesWithProvidersAndTrailers
                   .filter((movie) => movie.providers.length)
@@ -59,13 +77,20 @@ const App: React.FC = () => {
       .catch((error) => {
         console.error("Error fetching movies:", error);
       });
-  }, [filterParams]);
+  }, [filterParams, selectedCountryId]);
 
   return (
       <Container>
         <Row className="justify-content-md-center mb-3">
-          <Col md="8">
+          <Col md="6">
             <h1 >Pick Me a Movie</h1>
+          </Col>
+          <Col md="2" className="d-flex align-items-center">
+            <CountryFilter
+              countries={countryOptions}
+              selectedCountryId={selectedCountryId}
+              setSelectedCountryId={setSelectedCountryId}
+            />
           </Col>
         </Row>
         <Row className="justify-content-md-center mb-3">
---
+++ src/interfaces/countryInterface.ts
@@ -0,0 +1,7 @@
+interface Country {
+  iso_3166_1: string;
+  english_name: string;
+  native_name: string;
+}
+
+export type { Country };
