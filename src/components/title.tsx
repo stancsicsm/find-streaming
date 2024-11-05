4661e7bd9a0b4d34cc216269aae68253d6c5a8ee
Mate Stancsics <mate.stancsics@tngtech.com>
2024-11-05 22:13:13 +0100

    feat: add logo to title

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -2,9 +2,17 @@ import {Row, Col} from "react-bootstrap";
 
 const Title = () => {
   return (
-    <Row className="justify-content-md-center mt-2 mb-2">
+    <Row className="justify-content-md-center mt-2 mb-3">
       <Col>
-        <h1>Find Streaming</h1>
+        <div className="d-flex">
+          <img
+            src={process.env.PUBLIC_URL + "/logo.png"}
+            alt="logo"
+            height='40px'
+            className="rounded me-2"
+          />
+          <h1 className="my-auto">Find Streaming</h1>
+        </div>
       </Col>
     </Row>
   )
